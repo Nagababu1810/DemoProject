47c060882f0ac9bb51ae01b832e5cd14c971d37c
Nag <Nag@Nagababu>
2020-09-03 16:01:11 +0530

    first commit

---
+++ SampleProject/src/abcd/StringEx.java
@@ -0,0 +1,15 @@
+package abcd;
+
+public class StringEx {
+
+	public static void main(String[] args) {
+		
+		
+		String s1="nagababu";
+		System.out.println(s1);
+		System.out.println(s1.charAt(1));
+		
+
+	}
+
+}
