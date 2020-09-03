adc5104e42dafaa5734504344e116f24b99ac475
Nag <Nag@Nagababu>
2020-09-03 16:13:10 +0530

    add class Demo.java

---
+++ SampleProject/src/abcd/Demo.java
@@ -0,0 +1,14 @@
+package abcd;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=20;
+		
+		System.out.println("adding the two numbers:"+(a+b));
+
+	}
+
+}
