7256ea9416a3cc1c1229b6c4dc17cf6e51d18a1b
NagababuLabs <nagababu.tester18@gmail.com>
2021-03-29 12:13:19 +0530

    delete cookies

--- NewTask/src/test/java/TC_001.java
+++ NewTask/src/test/java/TC_001.java
@@ -14,11 +14,15 @@ public class TC_001 extends BaseTest {
 		driver.get("https://www.gmail.com");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+		
+		driver.manage().deleteAllCookies();
+		
 		driver.findElement(By.xpath("//input[@id='identifierId']")).sendKeys("mddnagababu1810");
 		
 		//driver.findElement(By.xpath("//span[text()='Next']")).click();
 		//driver.findElement(By.linkText("Next")).click();
 		driver.findElement(By.xpath("//*[@id=\"identifierNext\"]/div/button/span")).click();
+		
 	}
 
 }
